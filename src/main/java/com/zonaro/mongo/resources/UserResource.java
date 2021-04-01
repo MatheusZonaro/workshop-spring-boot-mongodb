77a8f3975cbe2cc35cd66da5dfcfdf2adc34b9b9
Matheus Zonaro <matheuszonaro@hotmail.com>
2021-04-01 10:53:52 -0300

    Third

--- src/main/java/com/zonaro/mongo/resources/UserResource.java
+++ src/main/java/com/zonaro/mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.zonaro.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zonaro.mongo.domain.User;
+import com.zonaro.mongo.dto.UserDTO;
 import com.zonaro.mongo.services.UserService;
 
 @RestController
@@ -19,14 +21,18 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method = RequestMethod.GET) // poderia ser tbm, requestmapping // Vai ser um get, pesquisa, obter info no padrao rest
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 
 
 		List<User> list = service.findAll();
 		
 		
-		return ResponseEntity.ok().body(list);
+		// Aqui eu pego o retorno da lista e com expressao lambda tranformo em listDto só com o que preciso
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		
+		
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
